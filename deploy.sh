044efe8ae70cf83def15b9ee287d4856604e8bfd
walter52 <walter52@sohu.com>
2019-08-22 20:44:13 +0800

    配置文件更新

--- deploy.sh
+++ deploy.sh
@@ -7,5 +7,15 @@
 #在vim中用"set ff=unix"切换格式或者使用"dos2unix"命令
 
 docker login --username=walter52@sohu.com registry.cn-shenzhen.aliyuncs.com
+if [ $? -eq 1 ]; then
+    echo "docker登录失败"
+    exit 0
+fi
 docker pull registry.cn-shenzhen.aliyuncs.com/walter_self/mid_nginx:master
-docker run -d --name min
+containerName='mid_nginx'
+existContainer=(`docker ps -a | grep "${containerName}"`)
+if [ -n "$existContainer" ]; then
+    docker stop $existContainer
+    docker container rm $existContainer
+fi
+docker run -d --name mid_nginx -d -p 80:80 registry.cn-shenzhen.aliyuncs.com/walter_self/mid_nginx:master
