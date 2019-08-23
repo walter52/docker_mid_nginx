a3f4eddba72d0c0355316e62b2753bded20a65d9
walter52 <walter52@sohu.com>
2019-08-23 14:47:26 +0800

    配置文件更新

--- mid_nginx_deploy.sh
+++ mid_nginx_deploy.sh
@@ -6,16 +6,25 @@
 #Version :v1.0
 #在vim中用"set ff=unix"切换格式或者使用"dos2unix"命令
 
-docker login --username=walter52@sohu.com registry.cn-shenzhen.aliyuncs.com
+dockerName='mid_nginx'
+dockerTag='master'
+host='registry.cn-shenzhen.aliyuncs.com/walter_self'
+dockerInfo=$host/$dockerName:$dockerTag
+
+docker pull $dockerInfo
 if [ $? -eq 1 ]; then
+    echo "docker登录"
+    docker login --username=walter52@sohu.com registry.cn-shenzhen.aliyuncs.com
+    if [ $? -eq 1 ]; then
     echo "docker登录失败"
     exit 0
+    fi
 fi
-docker pull registry.cn-shenzhen.aliyuncs.com/walter_self/mid_nginx:master
-containerName='mid_nginx'
-existContainer=(`docker ps -a | grep "${containerName}"`)
+
+existContainer=(`docker ps -a | grep "${dockerName}"`)
 if [ -n "$existContainer" ]; then
-    docker stop $existContainer
-    docker container rm $existContainer
+    docker stop $dockerName
+    docker container rm $dockerName
+    docker image rm $dockerInfo
 fi
-docker run -d --name mid_nginx -d -p 80:80 registry.cn-shenzhen.aliyuncs.com/walter_self/mid_nginx:master
+docker run -d --name $dockerName -d -p 80:80 $dockerInfo
